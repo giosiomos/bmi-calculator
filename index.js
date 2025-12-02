e94f592ac520ffa3633756ef1206d4422fcfc6fb
giosiomos <giosiomos99@protonmail.com>
2025-12-02 13:22:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-function bmiCalculator(weight, height) {
-    const bmi = weight / Math.pow(height, 2);
-    return Math.round(bmi);
+function bmiCalculator (weight, height) {
+    var bmi = weight / Math.pow(height, 2);
+    bmi = Math.round(bmi);
+    var interpretation; 
+    
+    if (bmi < 18.5) { 
+         interpretation = ("Your BMI is " + bmi + " , so you are underweight.")
+    }
+
+    else if (bmi >= 18.5 && bmi <= 24.9) {
+            interpretation = ("Your BMI is " + bmi + " , so you have a normal weight.")
+    }
+
+    else if (bmi > 24.9) {
+         interpretation = ("Your BMI is " + bmi + " , so you are overweight.")
+    }
+
+    return interpretation;
 }
-+
-console.log(bmiCalculator(65, 1.8));
+
+console.log(bmiCalculator(60, 2));
